09f1d7af0d90561c95bf024e4a14c5ab1a59c9b5
Dmitry Martynenko <dmitry.martynenko92@gmail.com>
2021-12-25 18:03:28 +0300

    create class Engine.java

---
+++ src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,84 @@
+package hexlet.code;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Engine {
+    public static int randomizer(int maxRandomNumber) {
+        Random random = new Random();
+        return random.nextInt(maxRandomNumber);
+    }
+    public static void startGame(int gameNumber) {
+        Cli.greetingUser();
+        final int rounds = 3;
+        final int evenGame = 2;
+        final int calcGame = 3;
+        int score = 0;
+        String correctAnswer;
+        if (gameNumber == evenGame) {
+            System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
+            for (int i = 0; i < rounds; i++) {
+                Even.even();
+                correctAnswer = "" + Even.getCorrectAmswer();
+                System.out.println("Question: " + Even.getRandomDigit());
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Your answer: ");
+                String userAnswer = sc.next();
+                if (userAnswer.equals(correctAnswer)) {
+                    score++;
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'"
+                            + userAnswer
+                            + "'"
+                            + " is wrong answer ;(. Correct answer was "
+                            + "'"
+                            + correctAnswer
+                            + "'"
+                            + "\nLet's try again, "
+                            + Cli.getUserName()
+                            + "!");
+                    break;
+                }
+                if (score == rounds) {
+                    System.out.println("Congratulations, "
+                            + Cli.getUserName()
+                            + "!");
+                }
+            }
+        }
+        if (gameNumber == calcGame) {
+            System.out.println("What is the result of the expression?");
+            for (int i = 0; i < rounds; i++) {
+                Calc.getRandomOperation();
+                correctAnswer = "" + Calc.getResult();
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Your answer: ");
+                String userAnswer = sc.next();
+                if (userAnswer.equals(correctAnswer)) {
+                    score++;
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'"
+                            + userAnswer
+                            + "'"
+                            + " is wrong answer ;(. Correct answer was "
+                            + "'"
+                            + correctAnswer
+                            + "'"
+                            + "\nLet's try again, "
+                            + Cli.getUserName()
+                            + "!");
+                    break;
+                }
+                if (score == rounds) {
+                    System.out.println("Congratulations, "
+                            + Cli.getUserName()
+                            + "!");
+                }
+            }
+        }
+    }
+}
