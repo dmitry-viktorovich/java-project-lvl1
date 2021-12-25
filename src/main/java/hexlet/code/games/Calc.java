6c9fe65c0abf23d53b9335495d135d15f99af3a4
Dmitry Martynenko <dmitry.martynenko92@gmail.com>
2021-12-25 18:29:45 +0300

    remove operation methods

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -4,50 +4,46 @@ import hexlet.code.Engine;
 
 public class Calc {
     private static int result;
-
+    private static int x;
+    private static int y;
+    private static String operation;
     public static void getRandomOperation() {
         final int randomOperationNumber = 3;
         final int randomDigit = 100;
         int randomOperation = Engine.randomizer(randomOperationNumber);
-        String question;
-        int x;
-        int y;
         if (randomOperation == 0) {
             x = Engine.randomizer(randomDigit);
             y = Engine.randomizer(randomDigit);
-            question = "Question: " + x + " + " + y;
-            System.out.println(question);
-            result = sum(x, y);
+            operation = " + ";
+            result = x + y;
         }
         if (randomOperation == 1) {
             x = Engine.randomizer(randomDigit);
             y = Engine.randomizer(randomDigit);
-            question = "Question: " + x + " - " + y;
-            System.out.println(question);
-            result = diff(x, y);
+            operation = " - ";
+            result = x - y;
         }
         if (randomOperation == 2) {
             x = Engine.randomizer(randomDigit);
             y = Engine.randomizer(randomDigit);
-            question = "Question: " + x + " * " + y;
-            System.out.println(question);
-            result = multiply(x, y);
+            operation = " * ";
+            result = x * y;
         }
-    }
-
-    private static int sum(int x, int y) {
-        return x + y;
-    }
-
-    private static int diff(int x, int y) {
-        return x - y;
-    }
-
-    private static int multiply(int x, int y) {
-        return x * y;
     }
 
     public static int getResult() {
         return result;
+    }
+
+    public static int getX() {
+        return x;
+    }
+
+    public static int getY() {
+        return y;
+    }
+
+    public static String getOperation() {
+        return operation;
     }
 }
