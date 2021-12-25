5f97094fbec18381b07c8403c28f77f5fcef3bb3
Dmitry Martynenko <dmitry.martynenko92@gmail.com>
2021-12-25 18:00:32 +0300

    Selection and start game through class Engine

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,24 +4,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter."
-            + "\n1 - Greet"
-            + "\n2 - Even"
-            + "\n0 - Exit");
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                0 - Exit""");
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Your choice: ");
         int gameNumber = sc.nextInt();
-
-        if (gameNumber == 1) {
-            System.out.println("\nWelcome to the Brain Games!");
-            Cli.greetingUser();
-        }
-
-        if (gameNumber == 2) {
-            System.out.println("\nWelcome to the Brain Games!");
-            Cli.greetingUser();
-            Even.runEvenGame();
-        }
+        Engine.startGame(gameNumber);
     }
 }
