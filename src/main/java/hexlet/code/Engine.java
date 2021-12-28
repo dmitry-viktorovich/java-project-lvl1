f15c2b57ce7d6b3184b8b7a624ce9297b3a9454a
Dmitry Martynenko <dmitry.martynenko92@gmail.com>
2021-12-29 00:09:16 +0300

    add Progression engine

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -2,6 +2,7 @@ package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -17,6 +18,7 @@ public class Engine {
         final int evenGame = 2;
         final int calcGame = 3;
         final int gcdGame = 4;
+        final int progression = 5;
         int score = 0;
         String correctAnswer;
         if (gameNumber == evenGame) {
@@ -89,6 +91,38 @@ public class Engine {
             for (int i = 0; i < rounds; i++) {
                 correctAnswer = "" + gcd.getGcd();
                 System.out.println("Question: " + gcd.getX() + " " + gcd.getY());
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Your answer: ");
+                String userAnswer = sc.next();
+                if (userAnswer.equals(correctAnswer)) {
+                    score++;
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'"
+                            + userAnswer
+                            + "'"
+                            + " is wrong answer ;(. Correct answer was "
+                            + "'"
+                            + correctAnswer
+                            + "'"
+                            + "\nLet's try again, "
+                            + Cli.getUserName()
+                            + "!");
+                    break;
+                }
+                if (score == rounds) {
+                    System.out.println("Congratulations, "
+                            + Cli.getUserName()
+                            + "!");
+                }
+            }
+        }
+        if (gameNumber == progression) {
+            System.out.println("What number is missing in the progression?");
+            for (int i = 0; i < rounds; i++) {
+                Progression prog = new Progression();
+                correctAnswer = "" + prog.getCorrectAnswer();
+                System.out.println("Question: " + prog.getProgression());
                 Scanner sc = new Scanner(System.in);
                 System.out.print("Your answer: ");
                 String userAnswer = sc.next();
