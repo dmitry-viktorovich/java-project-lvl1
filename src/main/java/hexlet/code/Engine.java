d4c7455be68a6d836d5d0fc5cd8c5f32526f2708
Dmitry Martynenko <dmitry.martynenko92@gmail.com>
2021-12-29 22:15:58 +0300

    add Prime game engine

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -1,9 +1,10 @@
 package hexlet.code;
+
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
-
+import hexlet.code.games.Prime;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,15 +14,24 @@ public class Engine {
         return random.nextInt(maxRandomNumber);
     }
     public static void startGame(int gameNumber) {
-        Cli.greetingUser();
         final int rounds = 3;
+        final int greet = 1;
         final int evenGame = 2;
         final int calcGame = 3;
         final int gcdGame = 4;
-        final int progression = 5;
+        final int progressionGame = 5;
+        final int primeGame = 6;
+        final int exit = 0;
         int score = 0;
         String correctAnswer;
+        if (gameNumber == exit) {
+            System.out.println("\nGoodbye!");
+        }
+        if (gameNumber == greet) {
+            Cli.greetingUser();
+        }
         if (gameNumber == evenGame) {
+            Cli.greetingUser();
             System.out.println("Answer 'yes' if number even otherwise answer 'no'.");
             for (int i = 0; i < rounds; i++) {
                 Even.even();
@@ -54,6 +64,7 @@ public class Engine {
             }
         }
         if (gameNumber == calcGame) {
+            Cli.greetingUser();
             System.out.println("What is the result of the expression?");
             for (int i = 0; i < rounds; i++) {
                 Calc.getRandomOperation();
@@ -86,6 +97,7 @@ public class Engine {
             }
         }
         if (gameNumber == gcdGame) {
+            Cli.greetingUser();
             System.out.println("Find the greatest common divisor of given numbers.");
             GCD gcd = new GCD();
             for (int i = 0; i < rounds; i++) {
@@ -117,12 +129,46 @@ public class Engine {
                 }
             }
         }
-        if (gameNumber == progression) {
+        if (gameNumber == progressionGame) {
+            Cli.greetingUser();
             System.out.println("What number is missing in the progression?");
             for (int i = 0; i < rounds; i++) {
                 Progression prog = new Progression();
                 correctAnswer = "" + prog.getCorrectAnswer();
                 System.out.println("Question: " + prog.getProgression());
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Your answer: ");
+                String userAnswer = sc.next();
+                if (userAnswer.equals(correctAnswer)) {
+                    score++;
+                    System.out.println("Correct!");
+                } else {
+                    System.out.println("'"
+                            + userAnswer
+                            + "'"
+                            + " is wrong answer ;(. Correct answer was "
+                            + "'"
+                            + correctAnswer
+                            + "'"
+                            + "\nLet's try again, "
+                            + Cli.getUserName()
+                            + "!");
+                    break;
+                }
+                if (score == rounds) {
+                    System.out.println("Congratulations, "
+                            + Cli.getUserName()
+                            + "!");
+                }
+            }
+        }
+        if (gameNumber == primeGame) {
+            Cli.greetingUser();
+            System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+            for (int i = 0; i < rounds; i++) {
+                Prime prime = new Prime();
+                correctAnswer = "" + prime.getCorrectAnswer();
+                System.out.println("Question: " + prime.getQuestion());
                 Scanner sc = new Scanner(System.in);
                 System.out.print("Your answer: ");
                 String userAnswer = sc.next();
